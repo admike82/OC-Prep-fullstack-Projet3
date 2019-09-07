b43cf57bf383194f798905d227fb2d86e2176b1e
admike82 <admike82@gmail.com>
2019-09-07 15:05:58 +0200

    Model Votes

---
+++ Lib/Vendors/Model/VotesManager.php
@@ -0,0 +1,45 @@
+<?php
+namespace Model;
+
+use \Fram\Manager;
+use \Entity\Vote;
+
+abstract class VotesManager extends Manager {
+    /**
+     * Méthode permettant d'enregistrer un vote.
+     * @param $vote Vote Le vote à enregistrer
+     * @return void
+     */
+    public function save(Vote $vote)
+    {
+        $this->add($vote);
+    }
+
+    /**
+     * Méthode permettant d'ajouter un vote.
+     * @param $vote Vote Le vote à ajouter
+     * @return void
+     */
+    abstract protected function add(Vote $vote);
+
+    /**
+     * Méthode permettant de supprimer un vote.
+     * @param $idVote int, $idActeur int Les identifiants de l'utilasteur et de l'acteur
+     * @return void
+     */
+    abstract public function delete(int $idUser, int $idActeur);
+
+    /**
+     * Méthode permettant d'obtenir un vote spécifique.
+     * @param $idVote int, $idActeur int Les identifiants de l'utilasteur et de l'acteur
+     * @return Vote
+     */
+    abstract public function get(int $idUser, int $idActeur);
+
+    /**
+     * Méthode permettant de récupérer une liste de vote.
+     * @param $idActeur int L'identifian de l'acteur sur lequel on veut récupérer les votes
+     * @return array
+     */
+    abstract public function getListOf(int $idActeur);
+}
---
+++ Lib/Vendors/Model/VotesManagerPDO.php
@@ -0,0 +1,43 @@
+<?php
+namespace Model;
+
+use \Entity\Vote;
+
+class VotesManagerPDO extends VotesManager {
+
+    protected function add(Vote $vote) {
+        $q = $this->dao->prepare('INSERT INTO vote SET id_user = :idUser, id_acteur = :idActeur, vote = :vote');
+
+        $q->bindValue(':idUser', $vote->idUser(), \PDO::PARAM_INT);
+        $q->bindValue(':idActeur', $vote->idActeur(), \PDO::PARAM_INT);
+        $q->bindValue(':vote', $vote->vote(), \PDO::PARAM_BOOL);
+
+        $q->execute();
+    }
+
+    public function delete(int $idUser, int $idActeur) {
+        $this->dao->exec('DELETE FROM vote WHERE id_user = '.$idUser.' AND id_acteur = '.$idActeur);
+    }
+
+    public function get(int $idUser, int $idActeur) {
+        $q = $this->dao->prepare('SELECT id_user, id_acteur, vote FROM vote WHERE id_user = :idUser AND id_acteur = :idActeur');
+        $q->bindValue(':idUser', $idUser, \PDO::PARAM_INT);
+        $q->bindValue(':idActeur', $idActeur, \PDO::PARAM_INT);
+        $q->execute();
+
+        $q->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Vote');
+
+        return $q->fetch();
+    }
+
+    public function getListOf(int $idActeur) {
+
+        $q = $this->dao->prepare('SELECT id_user, id_acteur, vote FROM vote WHERE id_acteur = :idActeur');
+        $q->bindValue(':idActeur', $idActeur, \PDO::PARAM_INT);
+        $q->execute();
+
+        $q->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Vote');
+
+        return $q->fetchAll();
+    }
+}
