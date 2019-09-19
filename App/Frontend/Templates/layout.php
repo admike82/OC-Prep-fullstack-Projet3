9e75c0662f078387ce6a384ccbf150a4aac3d071
admike82 <admike82@gmail.com>
2019-09-19 09:59:34 +0200

    modif viewport (apple)

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -7,7 +7,7 @@
     </title>
 
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="/css/style.css" type="text/css" />
